2c9ebf88c2dd99ebaa172406eb9ad27cb2061d95
dandrecampbell <dandrecampbell5@gmail.com>
2021-01-15 19:49:39 -0500

    landing page image

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
+import goals from './img/goals.svg';
 
 const aboutStyle = {
     width: "100vw",
@@ -9,7 +10,7 @@ const aboutStyle = {
 };
 
 const name = {
-    paddingTop: "8%",
+    paddingTop: "5%",
     fontSize: "70px",
     letterSpacing: "10px",
     textAlign: "center"
@@ -22,12 +23,21 @@ const title = {
     textAlign: "center"
 };
 
+const imgStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto",
+    width: "375px",
+    height: "auto"
+};
+
 const aboutText = {
     paddingTop: "5%",
-    width: "70%",
+    width: "87%",
     margin: "auto",
-    fontSize: "20px",
-    lineHeight: "1.8"
+    fontSize: "22px",
+    lineHeight: "2.2"
 };
 
 export default function About() {
@@ -36,14 +46,21 @@ export default function About() {
             <Card.Body>
                 <Card.Title style={name}>D'Andre Campbell</Card.Title>
                 <Card.Subtitle className="mb-2" style={title}>Developer & Photographer</Card.Subtitle>
-                <Card.Text style={aboutText}>
-                    Hey, my name is D’Andre. I’m a self taught, aspiring Front End Developer based out 
-                    of New York. I choose this area of development because it allows me to combine my 
-                    creative side with my love for coding. Currently, I’m working in the IT field as 
-                    a Technician but I’ve always had an interest in the Development field. I have 4 
-                    years of programming in various different languages such as <span style={{color:"#d9534f"}}>Java</span> and <span style={{color:"#9884fc"}}>Python</span> but 
-                    currently <span style={{color:"rgb(39, 221, 221)"}}>React.js</span> is my main focus.
-                </Card.Text>
+                <Row style={{paddingTop:"20px"}}>
+                    <Col lg="6" style={{paddingTop:"25px"}}>
+                        <Card.Text style={aboutText}>
+                            Hey, my name is D’Andre. I’m a self taught, aspiring Front End Developer based out 
+                            of New York. I choose this area of development because it allows me to combine my 
+                            creative side with my love for coding. Currently, I’m working in the IT field as 
+                            a Technician but I’ve always had an interest in the Development field. I have 4 
+                            years of programming in various different languages such as <span style={{color:"#d9534f"}}>Java</span> and <span style={{color:"#9884fc"}}>Python</span> but 
+                            currently <span style={{color:"rgb(39, 221, 221)"}}>React.js</span> is my main focus.
+                        </Card.Text>                    
+                    </Col>
+                    <Col lg="6" style={{paddingTop:"50px"}}>
+                        <Card.Img src={goals} alt="Goals Illustration" style={imgStyle} />
+                    </Col>
+                </Row>
             </Card.Body>
         </Card>
     )
