2f0d59693848244f2a67224ce60a784e4873ebff
dandrecampbell <dandrecampbell5@gmail.com>
2021-03-05 01:21:23 -0500

    not found page

--- src/components/notfound/NotFound.js
+++ src/components/notfound/NotFound.js
@@ -17,12 +17,12 @@ export default function NotFound() {
             position: "fixed"
         }}>
             <a href="/" style={{textDecoration: "none"}}>
-                <h1 style={{fontSize: "45px"}}>What are you looking for?</h1>
-            </a>
-            <img src={pic} alt="Productivity Illustration" width="275px" height="auto" style={coolImage} />
-            <h2 style={{fontSize: "24px", textAlign:"center", paddingTop:"30px"}}>
-                Page Not Available
-            </h2>
+                <h1 style={{fontSize: "45px"}}>Nothing to see here</h1>
+                <img src={pic} alt="Productivity Illustration" width="275px" height="auto" style={coolImage} />
+                <h2 style={{fontSize: "24px", textAlign:"center", paddingTop:"30px"}}>
+                    Page Not Available
+                </h2>
+            </a>            
         </div>
     )
 }
