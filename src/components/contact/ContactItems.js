e3764c5b22b96079481a3f82c47eaec6c02b40b3
dandrecampbell <dandrecampbell5@gmail.com>
2021-06-04 03:34:36 -0400

    rearrange contacts

--- src/components/contact/ContactItems.js
+++ src/components/contact/ContactItems.js
@@ -5,23 +5,23 @@ export const ContactList = [
         cName: 'fas fa-envelope contact-icon'
     },
     {
-        social: 'Discord',
-        url: 'https://discordapp.com/users/456580598480240672',
-        cName: 'fab fa-discord contact-icon'
-    },
+        social: 'Github',
+        url: 'https://github.com/DandreCampbell',
+        cName: 'fab fa-github contact-icon'
+    },    
     {
         social: 'LinkedIn',
         url: 'https://www.linkedin.com/in/dandre-campbell/',
         cName: 'fa fa-linkedin contact-icon'
-    },
-    {
-        social: 'Github',
-        url: 'https://github.com/DandreCampbell',
-        cName: 'fab fa-github contact-icon'
-    },
+    }, 
     {
         social: 'Instagram',
         url: 'https://www.instagram.com/dandre_campbell/',
         cName: 'fab fa-instagram contact-icon' 
-    }
+    },
+    {
+        social: 'Discord',
+        url: 'https://discordapp.com/users/456580598480240672',
+        cName: 'fab fa-discord contact-icon'
+    },
 ];
