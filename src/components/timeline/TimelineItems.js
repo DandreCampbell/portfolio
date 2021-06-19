99b087031e9477b0836a026c6e9d9632d33c6750
D'Andre Campbell <dandrecampbell5@gmail.com>
2021-06-19 14:16:30 -0400

    timeline changes

--- src/components/timeline/TimelineItems.js
+++ src/components/timeline/TimelineItems.js
@@ -1,5 +1,14 @@
 export const TimelineItems = [
     {
+        date: 'April 2021',
+        header: 'Web Developer & SEO Analyst',
+        where: 'TechneCo',
+        description: 'Design and Develop client websites using tools such as Figma, ' +
+        'Square, and Squarespace. Help improve a business\' online presence by conducting ' +
+        'research for keywords, alt text for images, monitor website traffic, and other strategies ' +
+        'to increase website traffic.'     
+    },
+    {
         date: 'May 2021',
         header: 'A+ Certification',
         where: '',
@@ -8,25 +17,25 @@ export const TimelineItems = [
     {
         date: 'October 2020',
         header: 'Security+ Certification',
-        where: ' ',
-        description: ' '
+        where: '',
+        description: ''
     },
     {
         date: 'May 2019',
         header: 'Technical Assisstant',
-        where: 'M&J Engineers: ',
+        where: 'M&J Engineers',
         description: 'Use remote access to provide technical support and customer assistance for 100+ employee. Set up and maintain personal computers for new hires.'
     },
     {
         date: 'March 2019',
         header: 'IT Field Technician',
-        where: 'CompuCom: ',
+        where: 'CompuCom',
         description: 'Travel to various Lowe’s locations to evaluate the state of different equipment. Diagnose and troubleshoot equipment performance issue.'
     },
     {
         date: 'May 2018',
         header: 'Networking Engineer Intern',
-        where: 'New York Community Bank:  ',
+        where: 'New York Community Bank',
         description: 'Handle cisco router and switch configurations using Putty or SecureCRT alongside various commands. Shadow engineers as they perform tasks such as fixing communication outages, monitoring network traffic, and troubleshooting devices.'
     }
 ];
